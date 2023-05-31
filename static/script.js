72b86fa388bdedcd7c7805c628ca3744705a903b
seulgi <rlatmfrl9@naver.com>
2023-06-01 02:08:21 +0900

    Refactor: 검색기능 부분 변수 합침

--- static/script.js
+++ static/script.js
@@ -14,9 +14,9 @@ fetch(
   .then((response) => response.json())
   .then((response) => {
     let movie_list = response['results'];
+    //document.getElementById('cards-box').innerHTML = '';
 
-    document.getElementById('cards-box').innerHTML = '';
-
+    let temp_html = ``;
     movie_list.forEach((i) => {
       let img_url = 'https://image.tmdb.org/t/p/w500' + i['backdrop_path'];
       let movie_title = i['title'];
@@ -24,7 +24,7 @@ fetch(
       let vote = i['vote_average'];
       let id = i['id'];
 
-      let temp_html = `
+      temp_html += `
       <div class="col">
         <a onclick="info_click(${id})" type="button">
           <img src='${img_url}' alt="">
@@ -38,8 +38,8 @@ fetch(
        </a>
     </div>`;
 
-      let cardsBoxId = document.querySelector('#cards-box');
-      cardsBoxId.insertAdjacentHTML('beforeend', temp_html); //Map, innerHTML
+      document.getElementById('cards-box').innerHTML = temp_html;
+      //cardsBoxId.insertAdjacentHTML('beforeend', temp_html); //Map, innerHTML
     });
   });
 
@@ -50,20 +50,21 @@ function info_click(id) {
 
 // 검색기능   // filter, forEach
 function search_btn() {
-  const name = document.getElementById('movie_name_input').value;
+  const movie_name_input = document.getElementById('movie_name_input').value;
 
   const card_arr = document.getElementsByClassName('col');
 
   const card_name_tag = [];
   const card_name_arr = [];
   for (let i = 0; i < card_arr.length; i++) {
-    card_name_tag[i] = card_arr[i].getElementsByTagName('h3');
-    card_name_arr[i] = card_name_tag[i][0].innerText;
+    card_name_arr[i] = card_arr[i].getElementsByTagName('h3')[0].innerText; //id값 주기
+    //card_name_arr[i] = card_name_tag[i][0].innerText;
   }
 
-  const movie_filter = (query) => {
+  const movie_filter = (movie_input) => {
+    //밖으로 빼기, 함수 안에 함수 X
     return card_name_arr.filter(
-      (el) => el.toUpperCase().indexOf(query.toUpperCase()) > -1
+      (el) => el.toUpperCase().indexOf(movie_input.toUpperCase()) > -1
     );
   };
 
@@ -71,15 +72,30 @@ function search_btn() {
     card_arr[i].style = 'display:none';
   }
 
-  let a = '';
+  let movie_name = '';
   for (let i = 0; i < card_name_arr.length; i++) {
-    a = movie_filter(name)[i];
-    //console.log(a);
+    movie_name = movie_filter(movie_name_input)[i];
     for (let i = 0; i < card_name_arr.length; i++) {
-      if (a === card_name_arr[i]) {
-        //console.log(card_name_arr[i], i);
+      if (movie_name === card_name_arr[i]) {
         card_arr[i].style = 'display:inline-block';
       }
     }
   }
 }
+// 버튼 스크롤
+function scroll_move() {
+  const homeBtn = document.querySelector('.home_btn');
+  const moviesBtn = document.querySelector('.movies_btn');
+
+  const location = document.querySelector('#movie_search_cards').offsetTop;
+
+  homeBtn.addEventListener('click', () =>
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  );
+
+  moviesBtn.addEventListener('click', () =>
+    window.scrollTo({ top: location, behavior: 'smooth' })
+  );
+}
+
+scroll_move();
